ae19cf6bf4fe7572b5f6a1ae2dd5deb8868ed1dd
Diego <diego2000avelar@gmail.com>
2021-04-03 20:50:49 -0600

    issue

--- node-utils/index.js
+++ node-utils/index.js
@@ -6,7 +6,7 @@ const sayMyName = name => console.log(`Your name is ${name}.`)
  * @param {*} number 
  */
 const numberFactorial = number => {
-    return number < 2 ? 1 : number * factorial2(number - 1);
+    return number < 2 ? 1 : number * numberFactorial(number - 1);
   };
 
 module.exports = {sayMyName, numberFactorial}
