3f9cc4acbf5f0daca835293672c351535ae4b82e
Diego <diego2000avelar@gmail.com>
2021-04-03 21:10:16 -0600

    flat array

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
-const {sayMyName, numberFactorial} = require("node-utils-package")
+const {numberFactorial, flattenElements} = require("node-utils-package")
+
+  const array = [[[0]], [1], [[[1]]]]
+ console.log(flattenElements(array))
 
 console.log(numberFactorial(5));
 
-sayMyName("Diego")
--- node-utils/index.js
+++ node-utils/index.js
@@ -1,12 +1,18 @@
-
-const sayMyName = name => console.log(`Your name is ${name}.`)
-
 /**
  * 
  * @param {*} number 
+ * return factorial from number
  */
 const numberFactorial = number => {
+    if(isNaN(number)) return null;
     return number < 2 ? 1 : number * numberFactorial(number - 1);
   };
 
-module.exports = {sayMyName, numberFactorial}
+  /**
+   * 
+   * @param {*} array 
+   * flat array into arrays
+   */
+const flattenElements = (array) => array.reduce((accumulator, element) => accumulator.concat(element), [])
+
+module.exports = {sayMyName, numberFactorial, flattenElements}
